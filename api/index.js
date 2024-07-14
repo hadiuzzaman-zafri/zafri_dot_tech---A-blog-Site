748cce19a8a7062b14725316dc03c436dcf7af71
MD HADIUZZAMAN ZAFRI <mdhadiuzzamanzafri@zafri.local>
2024-07-14 22:10:17 +0530

    connect the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log("MongoDB connection string:", process.env.MONGO); // Debugging line
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("The server is listening on port 3000!!");
+  console.log("The server is listening on port 3000!!");
 });
